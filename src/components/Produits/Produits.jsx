207ebd5297fde9c0f84789158f7957c588775198
mslnc93 <lounici.missipsa@outlook.fr>
2023-07-10 16:01:02 +0200

    style-card

--- src/components/Produits/Produits.jsx
+++ src/components/Produits/Produits.jsx
@@ -30,20 +30,33 @@ function Produits() {
         <div>
             <Navbar0 />
             <Navbar />
-            <div className='flex flex-wrap justify-center mx-auto w-3/4 space-x-4  space-y-4'>
-                {produits.map(produit => {
-                    return (
-                        <div className='text-center w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 p-4 shadow-md border border-gray-200 '>
-                            <div className="rounded overflow-hidden mb-4 flex justify-center items-center">
-                                <Link to='/'><img src={`http://localhost:5000/${produit.imagenom}`} alt={produit.nom} className="w-auto h-auto object-cover max-h-40" /></Link>
+            <div className="mx-auto max-w-6xl px-2 mt-12">
+                <div className="flex flex-wrap -mx-4">
+                    {produits.map((produit) => (
+                        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4">
+                            <div className="bg-white rounded-lg shadow-lg p-4 hover:scale-105 transition-transform duration-300 ease-in-out">
+                                <div className="overflow-hidden">
+                                    <Link to="/">
+                                        <img
+                                            src={`http://localhost:5000/${produit.imagenom}`}
+                                            alt={produit.nom}
+                                            className="w-full h-56 object-contain"
+                                        />
+                                    </Link>
+                                </div>
+                                <h4 className="text-xl font-bold mt-4">{produit.nom}</h4>
+                                <h2 className="text-lg font-medium">{produit.prix}</h2>
                             </div>
-                            <h4> {produit.nom} </h4>
-                            <h2> {produit.prix} </h2>
-
                         </div>
-                    )
-                })}
+                    ))}
+                </div>
             </div>
+
+
+
+
+
+
         </div>
     )
 }
